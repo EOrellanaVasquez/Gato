db8f285d2117c23be0e5475a5a6e3e3bb457af3b
Eduardo Orellana <123591780+EOrellanaVasquez@users.noreply.github.com>
2023-08-13 22:27:21 -0400

    1st commit
    Otro gato dijo el Chavo.

---
+++ src/components/Square/Square.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Square.css';
+import classnames from 'classnames';
+
+const Square = ({value, onClick, turn, winner}) => {
+    const handleClick = () =>{
+        (turn !== null && value === null) && onClick();
+    }
+
+    let squareClass = classnames({
+        square: true, 
+        [`square--${value}`]: value !== null,
+        winner: winner,
+    });
+
+  return (
+    <div className={squareClass} onClick={() => handleClick()}>
+      
+    </div>
+  )
+}
+
+export default Square
